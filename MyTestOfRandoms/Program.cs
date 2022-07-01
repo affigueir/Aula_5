afe36130c195871bd68c3324fd22226bc2ed2258
affigueir <affigueir@gmail.com>
2022-07-01 09:16:08 +0100

    First rnd part working

---
+++ MyTestOfRandoms/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Chronos
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many dice would you like to roll?");
+            int n = Int32.Parse(Console.ReadLine());
+
+            DiceRoll(n);
+        }
+        private static void DiceRoll(int x)
+            {
+                int[] dice = new int[x];
+
+                for(int i = 0; i < x; i++)
+                {
+                    Random random = new Random();
+
+                    dice[i] = random.Next(1, 7);
+                }
+                foreach (int item in dice)
+                {
+                    System.Console.WriteLine($"The roll is {item}");
+                }
+            }
+    }
+}    
